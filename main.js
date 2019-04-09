538fda8205aba7cd47508c33e47c419d5d743329
Clark Farriss <clarkfarriss@Clarks-MacBook-Pro.local>
2019-04-09 19:21:17 -0400

    updated port and contact page

--- main.js
+++ main.js
@@ -1,33 +1,4 @@
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyBmjUBa0qYSElQ_didlyGnUHY_4SMzVciY",
-    authDomain: "clarks-portfolio.firebaseapp.com",
-    databaseURL: "https://clarks-portfolio.firebaseio.com",
-    projectId: "clarks-portfolio",
-    storageBucket: "",
-    messagingSenderId: "238251766294"
-  };
-  firebase.initializeApp(config);
 
-var email = "";
-var subject = "";
-var message = "";
-
-$("#submitBtn").on("click", function (e) {
-    e.preventDefault();
-    email = $("#emailInput").val().trim();
-    subject = $("#subjectInput").val().trim();
-    message = $("#messageInput").val().trim();
-
-    //push data to firebase 
-    firebase.database().ref().push({
-        email: email,
-        subject: subject,
-        message: message,
-    })
-    //clear data field
-    $("#emailInput").val("");
-    $("#subjectInput").val("");
-    $("#messageInput").val("");
-    alert("Your Message has been sent")
-});
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+})
